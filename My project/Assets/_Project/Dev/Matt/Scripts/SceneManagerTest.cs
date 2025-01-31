b2954c20ad017cf6b522b9b395daee0de2be6730
BattlefieldGuy <matthewklijnstra@gmail.com>
2025-01-31 16:26:51 +0100

    extra's

--- My project/Assets/_Project/Dev/Matt/Scripts/SceneManagerTest.cs
+++ My project/Assets/_Project/Dev/Matt/Scripts/SceneManagerTest.cs
@@ -25,7 +25,7 @@ public class SceneManagerTest : MonoBehaviour
 
     private IEnumerator Wait()
     {
-        yield return new WaitForSeconds(0.3f);
+        yield return new WaitForSeconds(0.4f);
         UnityEngine.SceneManagement.SceneManager.LoadScene(Scene);
     }
 }
