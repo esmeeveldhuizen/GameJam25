5700e93145a84cd128511fef5415dd5ad367af69
BattlefieldGuy <matthewklijnstra@gmail.com>
2025-01-31 16:21:45 +0100

    Scene loading mechanic done & audio added

--- My project/Assets/_Project/Dev/Matt/Scripts/SceneManagerTest.cs
+++ My project/Assets/_Project/Dev/Matt/Scripts/SceneManagerTest.cs
@@ -1,17 +1,31 @@
+using System.Collections;
 using UnityEngine;
 
 public class SceneManagerTest : MonoBehaviour
 {
     public string Scene;
+    [SerializeField] private AudioSource audioSource;
+
+    private void Start()
+    {
+        if (audioSource == null)
+            audioSource = FindFirstObjectByType<AudioSource>();
+    }
 
     public void LoadScene()
     {
         if (Scene != null)
         {
-            Debug.Log(Scene);
-            UnityEngine.SceneManagement.SceneManager.LoadScene(Scene);
+            audioSource.Play();
+            StartCoroutine(Wait());
         }
         else if (Scene == null)
             Debug.Log("empty");
+    }
+
+    private IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(0.3f);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(Scene);
     }
 }
