08554935cacb8d250e981d8c620a14f9e67958f2
esmée veldhuizen <esmee.veldhuizen@outlook.com>
2025-02-01 11:50:41 +0100

    MainPage done!

--- My project/Assets/_Project/Dev/Esmee/Scripts/InsertCode.cs
+++ My project/Assets/_Project/Dev/Esmee/Scripts/InsertCode.cs
@@ -5,16 +5,24 @@ using UnityEngine.SceneManagement;
 public class InsertCode : MonoBehaviour
 {
     public TMP_InputField input;
-    private string answer = "mrbubbles";
+    private string answer2 = "mrbubbles";
+    private string answer1 = "mutation";
+
     [SerializeField] private AudioSource confirmaudio;
 
     public void CheckAnswer()
     {
         string inputText = input.text;
 
-        if (inputText == answer)
+        if (inputText == answer1)
         {
             SceneManager.LoadScene("InfectedFish");
+            confirmaudio.Play();
+        }
+
+        if (inputText == answer2)
+        {
+            SceneManager.LoadScene("StartPage");
             confirmaudio.Play();
         }
     }
