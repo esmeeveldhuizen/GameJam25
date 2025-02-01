5e0de2890434b1b827e29e027e655e82d94ae037
esmée veldhuizen <esmee.veldhuizen@outlook.com>
2025-02-01 03:45:50 +0100

    Goodnight

---
+++ My project/Assets/_Project/Dev/Esmee/Scripts/SceneOrder.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+//scene management
+
+public class SceneOrder : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    //public void 
+    //op button load scene
+    //voor elk ig want niemand zin om na te denken 
+}
