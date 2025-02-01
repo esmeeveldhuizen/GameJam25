950454dbb1a25c3c7af1feb244eb19926e247fce
esmée veldhuizen <esmee.veldhuizen@outlook.com>
2025-02-01 12:23:57 +0100

    fish website works

--- My project/Assets/_Project/Dev/Esmee/Scripts/Minigames/LeaveGame.cs
+++ My project/Assets/_Project/Dev/Esmee/Scripts/Minigames/LeaveGame.cs
@@ -1,6 +1,15 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RetryGame : MonoBehaviour
 {
+    public void GoBack()
+    {
+        SceneManager.LoadScene("FishPage");
+    }
 
+    public void GoHome()
+    {
+        SceneManager.LoadScene("StartPage");
+    }
 }
