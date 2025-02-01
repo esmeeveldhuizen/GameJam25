6554ac99bb84c57aebac4d5c757283495a0a9e33
esmée veldhuizen <esmee.veldhuizen@outlook.com>
2025-02-01 07:19:21 +0100

    Goodmorning! adding the scenes (finally)

--- My project/Assets/_Project/Dev/Esmee/Scripts/SceneOrder.cs
+++ My project/Assets/_Project/Dev/Esmee/Scripts/SceneOrder.cs
@@ -1,19 +1,34 @@
 using UnityEngine;
-//scene management
+using UnityEngine.SceneManagement;
 
 public class SceneOrder : MonoBehaviour
 {
-    void Start()
-    {
+    //I'm sorry for this painfull to watch script, I am TIRED
 
+    public void Scene0()
+    {
+        SceneManager.LoadScene("AboutUs");
+    }
+    public void Scene1()
+    {
+        SceneManager.LoadScene("GeneralQuestions");
+    }
+    public void Scene2()
+    {
+        SceneManager.LoadScene("AqueHistory");
+    }
+    public void Scene3()
+    {
+        SceneManager.LoadScene("Blogs");
+    }
+    public void Scene4()
+    {
+        SceneManager.LoadScene("Photos-1");
+    }
+    public void Scene5()
+    {
+        SceneManager.LoadScene("Photos-2");
     }
 
-    void Update()
-    {
 
-    }
-
-    //public void 
-    //op button load scene
-    //voor elk ig want niemand zin om na te denken 
 }
